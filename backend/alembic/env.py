a277d91182dd25cca944882a052e8ede696d13ef
slick-v <ronn.proff@gmail.com>
2026-07-22 17:19:23 +0530

    push

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -3,7 +3,7 @@ import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from backend.app.core.config import settings
+from app.core.config import settings
 from app.models import User,Expense,Income
 from app.db.session import Base
 
